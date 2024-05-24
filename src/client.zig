070b95a85b6efc5f492391c07aca11cd6b584b3c
xfxpositions <xfxpositions@gmail.com>
2024-05-24 19:02:43 +0300

    ray_id implemented options added

--- src/client.zig
+++ src/client.zig
@@ -16,6 +16,8 @@ const Request = @import("./request.zig").Request;
 const Response = @import("./response.zig").Response;
 
 const ReadError = @import("./common.zig").ReadError;
+const generate_uuid = @import("./utils/uuid.zig").generate_uuid;
+const ServerOptions = @import("./server.zig").ServerOptions;
 
 /// A Client is an object that will store a stream to a client
 /// and return Request objects.
@@ -24,22 +26,25 @@ pub const Client = struct {
     allocator: Allocator,
 
     // Address of the client
-    remote_addr: net.Address,
+    remote_addr: net.Ip4Address,
 
     // Is it a safe connection?
     safe: bool,
 
     stream: net.Stream,
 
+    options: ServerOptions,
+
     // set to true if we know that the previous request is the last one
     no_more_requests: bool,
 
-    pub fn init(allocator: Allocator, connection: net.Server.Connection) !*Client {
+    pub fn init(allocator: Allocator, connection: net.Server.Connection, options: ServerOptions) !*Client {
         var self: *Client = try allocator.create(Client);
         self.allocator = allocator;
 
-        self.remote_addr = connection.address;
+        self.remote_addr = net.Ip4Address;
         self.stream = connection.stream;
+        self.options = options;
 
         return self;
     }
@@ -53,7 +58,12 @@ pub const Client = struct {
             return null;
         }
 
-        while (true) {}
+        var request: Request = undefined;
+
+        if (self.options.give_id_to_request) {
+            request.ray_id = generate_uuid();
+        }
+        return request;
     }
     pub fn read(self: *Client) ReadError!Request {
         _ = self;
