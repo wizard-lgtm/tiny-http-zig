e12fb1fb9df04ec1d3516542d9ec51a9bfa31f2e
xfxpositions <xfxpositions@gmail.com>
2024-05-24 19:20:55 +0300

    read_stream function added

--- src/client.zig
+++ src/client.zig
@@ -42,7 +42,7 @@ pub const Client = struct {
         var self: *Client = try allocator.create(Client);
         self.allocator = allocator;
 
-        self.remote_addr = net.Ip4Address;
+        self.remote_addr = connection.address.in;
         self.stream = connection.stream;
         self.options = options;
 
@@ -58,14 +58,49 @@ pub const Client = struct {
             return null;
         }
 
-        var request: Request = undefined;
+        const buffer = try self.read_stream();
+        var request = try Request.init(buffer, self.allocator, false, self.remote_addr, self.safe);
 
         if (self.options.give_id_to_request) {
             request.ray_id = generate_uuid();
         }
         return request;
     }
-    pub fn read(self: *Client) ReadError!Request {
-        _ = self;
+    ///
+    /// Reading request buffer
+    /// Returns a string
+    ///
+    pub fn read_stream(self: *Client) ReadError![]u8 {
+        const chunk_size: usize = 256;
+        var chunk_count: u16 = 1; // How many chunks are allocated
+        var total_read: usize = 0;
+        var bytes_read: usize = 0;
+
+        // allocate buffer for read
+        var buffer = try self.allocator(u8, chunk_size);
+
+        //Read with chunks
+        while (true) {
+            bytes_read = try self.stream.read(buffer[total_read..]);
+            if (bytes_read == 0) {
+                break; // End of the stream
+            }
+
+            total_read += bytes_read;
+
+            std.debug.print("bytes read: {}\n", .{bytes_read});
+            std.debug.print("total len of buffer: {}\n", .{buffer.len});
+
+            // Check if we need more size
+            if (total_read >= buffer.len) {
+                // Extend buffer size
+                chunk_count += 1;
+                buffer = try self.allocator.realloc(buffer, chunk_size * chunk_count);
+            } else {
+                break; // All data read
+            }
+        }
+
+        return buffer;
     }
 };
