0e884d126352ce054755ac22d3c558b55fc0296f
xfxpositions <xfxpositions@gmail.com>
2024-05-24 17:30:47 +0300

    new test functions

--- test.zig
+++ test.zig
@@ -2,10 +2,9 @@ const std = @import("std");
 const common = @import("./src/common.zig");
 const Header = common.Header;
 
-test "test strict headers" {
-    const e = error.InvalidHeader;
+const Request = @import("./src/request.zig").Request;
 
-    _ = e;
+test "test strict headers" {
 
     // These should not throw an error (valid headers)
     std.debug.print("{any}\n", .{Header.parse("Transfer-Encoding: chunked") != error.InvalidHeader});
@@ -19,4 +18,20 @@ test "test strict headers" {
     // try std.testing.expect(Header.parse(" Transfer-Encoding: chunked") == e); // leading space
     // try std.testing.expect(Header.parse("Transfer Encoding: chunked") == e); // missing hyphen
     // try std.testing.expect(Header.parse(" Transfer\tEncoding : chunked") == e); // spaces and tab characters
+}
+
+test "init request" {
+    const allocator = std.testing.allocator;
+    const raw_request =
+        "GET / HTTP/1.1\r\n" ++
+        "Host: example.com\r\n" ++
+        "User-Agent: curl/7.64.1\r\n" ++
+        "Accept: */*\r\n" ++
+        "\r\n" ++
+        "Hi, it's body";
+    const fake_addr = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 45944);
+    var request = try Request.init(raw_request, allocator, false, fake_addr, false);
+
+    std.debug.print("request created:", .{});
+    request.fmt();
 }
