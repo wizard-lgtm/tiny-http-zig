bea3fd58f8733e3759bc3d284e6f848100cb5417
xfxpositions <xfxpositions@gmail.com>
2024-05-25 16:28:12 +0300

    main updated

--- src/main.zig
+++ src/main.zig
@@ -11,7 +11,8 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const server = try Server.init(allocator, "127.0.0.1", 4000);
+    const server = try Server.init(allocator, "127.0.0.1", 4000, null);
+    defer _ = server.deinit();
 
     _ = try server.listen_http(null);
     while (true) {
@@ -19,15 +20,14 @@ pub fn main() !void {
         defer _ = request.deinit();
 
         std.debug.print("New request came up!\n", .{});
-        request.fmt();
 
         const response = try Response.init(allocator);
         defer _ = response.deinit();
 
         response.body = "Kirwe";
         response.status_code = common.StatusCode.init(200);
-        response.headers.append(common.Header.parse("Content-Type: text/plain"));
 
+        _ = try response.headers.append(try common.Header.parse("Content-Type: text/plain"));
         _ = try request.respond(response);
 
         std.debug.print("Response Sent!\n", .{});
