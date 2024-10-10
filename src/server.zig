1f5e76394c6391493c89c6d76fd9876838683837
wizard-lgtm <rippiten23@gmail.com>
2024-10-10 03:19:04 +0300

    Refactor Server Initialization and Handler Implementation
    - Moved server initialization parameters to the `ServerOptions` struct for improved organization and clarity.
    - Implemented basic functionality for the request handler to handle incoming requests.
    - Prepared codebase for migration to Zig version 0.14.0, ensuring compatibility with upcoming changes.
    
    This commit enhances code maintainability and sets the stage for future improvements.

--- src/server.zig
+++ src/server.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const common = @import("./common.zig");
 const net = std.net;
 
 const Address = net.Address;
@@ -8,20 +9,51 @@ const Client = @import("./client.zig").Client;
 
 const Listener = struct { addr: net.Address };
 
+///
+/// Server configs, you may configure the server before initializiton
+/// Functions:
+/// default -> returns default usage config
+///
 pub const ServerOptions = struct {
+    const Self = @This(); // Self binding
+    // Default options
+    const DefaultServerOptions = ServerOptions{
+        .give_id_to_request = true,
+        .logging = false, // !TODO not implemented!
+        .on_request_handler = null,
+        .addr = "127.0.0.1",
+        .port = 4000,
+    };
     // gives random uuid to each request
     give_id_to_request: bool,
 
     // Future planned
     logging: bool,
+
+    on_request_handler: ?*const common.Handler,
+    addr: []const u8,
+    port: u16,
+    pub fn default() ServerOptions {
+        return DefaultServerOptions;
+    }
 };
 
-pub const DefaultServerOptions = ServerOptions{ .give_id_to_request = true, .logging = false };
-
+// Define default options for listening on a network address
 const default_listen_options = net.Address.ListenOptions{
+    // Allow the socket to be bound to a port that is in a TIME_WAIT state
     .reuse_address = true,
+
+    // Allow multiple sockets to listen on the same port
     .reuse_port = true,
 };
+
+/// just a basic handler wrapper for threading
+fn handler_wrapper(h: common.Handler) void() {
+    // Execute the handler and handle error with catch
+    h() catch |err| {
+        std.debug.print("some error happened while executing handler! {any}\n", .{err});
+    };
+}
 
 pub const Server = struct {
     allocator: std.mem.Allocator,
@@ -29,16 +61,16 @@ pub const Server = struct {
     responder: net.Server,
     options: ServerOptions,
 
-    pub fn init(allocator: std.mem.Allocator, addr: []const u8, port: u16, options: ?ServerOptions) !*Server {
+    pub fn init(allocator: std.mem.Allocator, options: ?ServerOptions) !*Server {
         var self = try allocator.create(Server);
-        self.allocator = allocator;
-        self.listener = Listener{ .addr = try net.Address.parseIp4(addr, port) };
         if (options) |value| {
             self.options = value;
         } else {
             // Use defaults
-            self.options = DefaultServerOptions;
+            self.options = ServerOptions.default();
         }
+        self.allocator = allocator;
+        self.listener = Listener{ .addr = try net.Address.parseIp4(self.options.addr, self.options.port) };
         return self;
     }
     pub fn deinit(self: *Server) void {
